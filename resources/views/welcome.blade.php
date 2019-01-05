123bc7b8892dac7dc9d720c2771f958fc7358308
marquizq <michaelmallya@gmail.com>
2019-01-05 04:25:42 +0300

    Changes on welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    This is Awesome
                 </div>
 
                 <div class="links">
